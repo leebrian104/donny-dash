f90e4e0fd43c6918217defdd1d9e3c644b347676
Brian <leebrian104@gmail.com>
2022-08-01 00:10:31 -0400

    added scoreboard function

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let character = document.getElementById("character");
 let block = document.getElementById("block");
+let counter = 0;
 
 function jump(){
     if(character.classList == "animate"){return}
@@ -14,7 +15,12 @@ let checkHit = setInterval(function(){
     let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
     if(blockLeft < 20 && blockLeft > -20 && characterTop >= 130){
         block.style.animation = "none";
-        alert("Game Over.");
+        alert("Game Over! Score: " + Math.floor(counter/100));
+        counter = 0;
         block.style.animation = "block 1s infinite linear";
+    }
+    else{
+        counter++;
+        document.getElementById("score").innerHTML = Math.floor(counter/100);
     }
 },10)
